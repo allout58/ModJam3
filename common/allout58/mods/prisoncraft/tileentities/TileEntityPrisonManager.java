93c6aa299cd8f613c9d8cfd9cf1336deea549c80
James Hollowell <jamesthollowell@gmail.com>
2013-12-13 16:47:19 -0500

    Automatic commit 26

--- common/allout58/mods/prisoncraft/tileentities/TileEntityPrisonManager.java
+++ common/allout58/mods/prisoncraft/tileentities/TileEntityPrisonManager.java
@@ -1,15 +1,24 @@
 package allout58.mods.prisoncraft.tileentities;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.Packet132TileEntityData;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityPrisonManager extends TileEntity implements IInventory
 {
     private ItemStack[] playerInventory;
-    public static final int IVENTORY_SIZE=1;
+    public static final int IVENTORY_SIZE=40;
+    
+    public static final int START_MAIN=0;
+    public static final int START_HOTBAR=32; //??
+    public static final int START_ARMOR=36; //??
     
     public TileEntityPrisonManager()
     {
@@ -28,7 +37,65 @@ public class TileEntityPrisonManager extends TileEntity implements IInventory
         //loop through each block - todo... ;)
         
     }
+    
+    public void jailPlayer(EntityPlayerMP player)
+    {
+        //Take their inventory
+        
+    }
 
+    @Override
+    public void readFromNBT(NBTTagCompound tags)
+    {
+        NBTTagList tagList = tags.getTagList("Items");
+        playerInventory = new ItemStack[this.getSizeInventory()];
+        for (int i = 0; i < tagList.tagCount(); ++i)
+        {
+            NBTTagCompound tagCompound = (NBTTagCompound) tagList.tagAt(i);
+            byte slot = tagCompound.getByte("Slot");
+            if (slot >= 0 && slot < playerInventory.length)
+            {
+                playerInventory[slot] = ItemStack.loadItemStackFromNBT(tagCompound);
+            }
+        }
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tags)
+    {
+        // Write the ItemStacks in the inventory to NBT
+        NBTTagList tagList = new NBTTagList();
+        for (int currentIndex = 0; currentIndex < playerInventory.length; ++currentIndex)
+        {
+            if (playerInventory[currentIndex] != null)
+            {
+                NBTTagCompound tagCompound = new NBTTagCompound();
+                tagCompound.setByte("Slot", (byte) currentIndex);
+                playerInventory[currentIndex].writeToNBT(tagCompound);
+                tagList.appendTag(tagCompound);
+            }
+        }
+        tags.setTag("Items", tagList);
+
+    }
+    
+    /* Packets */
+    @Override
+    public Packet getDescriptionPacket()
+    {
+        NBTTagCompound tag = new NBTTagCompound();
+        writeToNBT(tag);
+        return new Packet132TileEntityData(xCoord, yCoord, zCoord, 1, tag);
+    }
+
+    @Override
+    public void onDataPacket(INetworkManager net, Packet132TileEntityData packet)
+    {
+        readFromNBT(packet.data);
+        worldObj.markBlockForRenderUpdate(xCoord, yCoord, zCoord);
+    }
+    
+    /*Inventory*/
     @Override
     public int getSizeInventory()
     {
@@ -62,20 +129,6 @@ public class TileEntityPrisonManager extends TileEntity implements IInventory
         }
 
         return itemStack;
-    }
-    
-    @Override
-    public void readFromNBT(NBTTagCompound nbttagcompound)
-    {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void writeToNBT(NBTTagCompound nbttagcompound)
-    {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
