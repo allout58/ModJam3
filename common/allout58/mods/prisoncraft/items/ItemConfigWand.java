a2e9dd32fac814d7ad56ee1342a9c697b6117bcd
James Hollowell <jamesthollowell@gmail.com>
2013-12-13 01:56:15 -0500

    Automatic commit 8

--- common/allout58/mods/prisoncraft/items/ItemConfigWand.java
+++ common/allout58/mods/prisoncraft/items/ItemConfigWand.java
@@ -3,6 +3,7 @@ package allout58.mods.prisoncraft.items;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 public class ItemConfigWand extends Item
@@ -19,10 +20,30 @@ public class ItemConfigWand extends Item
     {
         if (player.isSneaking())
         {
+            //reset
             stack.stackTagCompound = null;
-            this.setDamage(stack, 0);
         }
         return stack;
+    }
+    
+    @Override
+    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
+    {
+        if(!stack.stackTagCompound.hasKey("x1"))
+        {
+            stack.stackTagCompound.setInteger("x1", x);
+            stack.stackTagCompound.setInteger("y1", y);
+            stack.stackTagCompound.setInteger("z1", z);
+        }
+        else if(!stack.stackTagCompound.hasKey("x2"))
+        {
+            stack.stackTagCompound.setInteger("x2", x);
+            stack.stackTagCompound.setInteger("y2", y);
+            stack.stackTagCompound.setInteger("z2", z);
+            System.out.println("1:{" + stack.stackTagCompound.getInteger("x1") +", " + stack.stackTagCompound.getInteger("y1") +", "+ stack.stackTagCompound.getInteger("z1")+"} 2:{"+stack.stackTagCompound.getInteger("x2")+", "+stack.stackTagCompound.getInteger("y2")+", "+stack.stackTagCompound.getInteger("z2")+"}");
+        }
+        
+        return true;
     }
 
 }
