6f482d5d0ab50bede4dc2efce3e94406cb64915e
James Hollowell <jamesthollowell@gmail.com>
2013-12-15 20:09:05 -0500

    Automatic commit 94

--- common/allout58/mods/prisoncraft/commands/JailPermissions.java
+++ common/allout58/mods/prisoncraft/commands/JailPermissions.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class JailPermissions
 {
@@ -12,7 +13,21 @@ public class JailPermissions
     
     public boolean playerCanUse(ICommandSender sender)
     {
-//        for(int i=0;i<canUse.)
-        return true;
+        for(int i=0;i<canUse.size();i++)
+        {
+            if(((String)canUse.get(i)).equalsIgnoreCase(sender.getCommandSenderName()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void addUserPlayer(EntityPlayer player)
+    {
+        if(!playerCanUse(player))
+        {
+            canUse.add(player.username);
+        }
     }
 }
